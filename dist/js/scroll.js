878caeffb212cb96d1085b567600b3756e1ed0ce
Dogender <kwiatekbartosz84@gmail.com>
2022-09-15 21:33:28 +0200

    OmegaPC

---
+++ dist/js/scroll.js
@@ -0,0 +1,18 @@
+//Animacje Scroll-a
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky1", window.scrollY > 900);
+
+    
+    var com2 = document.querySelector(".comunicat4");
+    com2.classList.toggle("fadeIn", window.scrollY > 800);
+})
+
+
+
+
+
+//Autoscroll
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
